9ec7b9da2e470f89787c799378124fa35c954fc0
Bunga Mungil <bungamungil@icloud.com>
2024-06-11 14:04:00 +0700

    Add configuration to skip certificate verification database

--- Sources/Parfait/database.swift
+++ Sources/Parfait/database.swift
@@ -9,11 +9,27 @@ func databaseConfig() -> DatabaseConfigurationFactory {
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "parfait_db_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "parfait_db_password",
-        database: Environment.get("DATABASE_NAME") ?? "parfait_db_database"
+        database: Environment.get("DATABASE_NAME") ?? "parfait_db_database",
+        tlsConfiguration: Environment.get("DATABASE_SKIP_CERTIFICATE_VERIFICATION") == nil ? .standard() : .noCertificateVerification()
     )
 }
 
 
 func databaseID() -> DatabaseID {
     .mysql
+}
+
+
+extension TLSConfiguration {
+    
+    static func standard() -> TLSConfiguration {
+        return .makeClientConfiguration()
+    }
+    
+    static func noCertificateVerification() -> TLSConfiguration {
+        var tlsConfiguration: TLSConfiguration = .makeClientConfiguration()
+        tlsConfiguration.certificateVerification = .none
+        return tlsConfiguration
+    }
+    
 }
